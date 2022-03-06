28c9080d86c767feebccf8add16e75d212e98cbb
remant <remantmahato9798@gmail.com>
2022-03-06 21:05:19 +0530

    Added : UC-3 Part Time Employee Wage

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -1,22 +1,35 @@
 ﻿
 
 int Present = 1;
+int FullTime = 1;
 int Emp_per_rate = 20;
 int EmpWage = 0;
-int Emphr = 0;
+int EmpHr = 0;
 Random random = new Random();
+Random random1 = new Random();
 int Employeestatus = random.Next(0, 2);
+int Employee_Time=random1.Next(0, 2);
 if (Employeestatus == Present)
 {
-    Console.WriteLine("Employee is Present");
-    Emphr = 8;
+    if(Employee_Time == FullTime)
+    {
+        Console.WriteLine("Employee is Present");
+        EmpHr = 8;
+    }
+    else
+    {
+        Console.WriteLine("Employee is Part Time");
+        EmpHr = 4;
+
+
+    }
 }
 else
 {
     Console.WriteLine("Employee is absent");
-    Emphr = 0;
+    EmpHr = 0;
 }
 
-EmpWage = Emp_per_rate * Emphr;
+EmpWage = Emp_per_rate * EmpHr;
 Console.WriteLine("Employee daily wage is " + EmpWage);
 
