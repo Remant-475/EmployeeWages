a05e7ed77a2c0c53d277c6df929d2990c5dd8edd
remant <remantmahato9798@gmail.com>
2022-03-06 20:45:37 +0530

    Added : UC-2 Employee Daily Wage

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -1,14 +1,22 @@
 ﻿
 
 int Present = 1;
+int Emp_per_rate = 20;
+int EmpWage = 0;
+int Emphr = 0;
 Random random = new Random();
 int Employeestatus = random.Next(0, 2);
 if (Employeestatus == Present)
 {
     Console.WriteLine("Employee is Present");
+    Emphr = 8;
 }
 else
 {
     Console.WriteLine("Employee is absent");
+    Emphr = 0;
 }
+
+EmpWage = Emp_per_rate * Emphr;
+Console.WriteLine("Employee daily wage is " + EmpWage);
 
