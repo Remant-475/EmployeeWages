31541dcf43020b98b974f3cc650adc3ec328ced8
remant <remantmahato9798@gmail.com>
2022-03-06 20:27:13 +0530

    Added : UC-1 Employee Attendance

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Employee Wage Computation Program");
+﻿
+
+int Present = 1;
+Random random = new Random();
+int Employeestatus = random.Next(0, 2);
+if (Employeestatus == Present)
+{
+    Console.WriteLine("Employee is Present");
+}
+else
+{
+    Console.WriteLine("Employee is absent");
+}
+
