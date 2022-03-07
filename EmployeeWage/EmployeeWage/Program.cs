cd9d04f3806d7c79027857d97a32713e3f495dd9
remant <remantmahato9798@gmail.com>
2022-03-07 23:10:22 +0530

    Added : UC5-EmployeeWagesMonth

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -1,27 +1,54 @@
 ﻿
 
-int Emp_per_rate = 20;
-int EmpWage = 0;
-int EmpHr = 0;
-Random random = new Random();
-Random random1 = new Random();
-int EmployeeAttendance = random.Next(0, 3);
 
-switch (EmployeeAttendance)
+
+namespace EmployeeWage
 {
-    case 1:
-        Console.WriteLine("Employee is Present ");
-        EmpHr = 8;
-        break;
-    case 2:
-        Console.WriteLine("Part Timer is Present");
-        EmpHr = 4;
-        break;
-    default:
-        Console.WriteLine("Employee is Absent ");
-        break;
+    class program
+    {
+        public const int Full_Time = 1;
+        public const int Part_time = 2;
+        public const int Emp_Hr_Per_Rate = 20;
+        public const int Num_of_Working_Days = 20;
+
+        public static void Main(String[] args)
+
+        {
+            Console.WriteLine("Welcome to Employee Wage Calculation");
+            int empHr = 0;
+            int empwage = 0;
+            int totalEmpwage = 0;
+            for (int day = 0; day < Num_of_Working_Days; day++)
+            {
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case Part_time:
+                        empHr = 4;
+                        break;
+                    case Full_Time:
+                        empHr = 8;
+                        break;
+                    default:
+                        empHr = 0;
+                        break;
+                }
+                empwage = (Emp_Hr_Per_Rate * empHr);
+                totalEmpwage += empwage;
+                Console.WriteLine("Daily Wage is :" + empwage);
+
+            }
+            Console.WriteLine("Total Wage:" + totalEmpwage);
+        }
+    }
 }
 
-EmpWage = Emp_per_rate * EmpHr;
-Console.WriteLine("Employee daily wage is " + EmpWage);
+
+
+
+
+
+
+
 
